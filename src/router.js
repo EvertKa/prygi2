345b75c95f3261162f37b01b07999b5324e9fb74
Matu16 <>
2023-09-22 11:00:15 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -12,6 +12,60 @@ const routes = [
     name: "biolagunevad",
     component: () => import("../src/pages/bio.vue"),
   },
+
+  {
+    path: "/bioaia",
+    name: "biolagunevadaia",
+    component: () => import("../src/pages/bioaia.vue"),
+  },
+
+  {
+    path: "/klaas",
+    name: "klaas",
+    component: () => import("../src/pages/klaas.vue"),
+  },
+
+  {
+    path: "/milleks-sorteerida",
+    name: "milleks",
+    component: () => import("../src/pages/milleks-sorteerida.vue"),
+  },
+
+  {
+    path: "/ohtlikud",
+    name: "ohtlikud",
+    component: () => import("../src/pages/ohtlikud.vue"),
+  },
+
+  {
+    path: "/paber",
+    name: "paber",
+    component: () => import("../src/pages/paber.vue"),
+  },
+
+  {
+    path: "/plast",
+    name: "plast",
+    component: () => import("../src/pages/plast.vue"),
+  },
+
+  {
+    path: "/tekstiilkolbmatu",
+    name: "tekstiilkolbmatu",
+    component: () => import("../src/pages/tekstiilkolbmatu.vue"),
+  },
+
+  {
+    path: "/tekstiil",
+    name: "tekstiil",
+    component: () => import("../src/pages/tesktiil.vue"),
+  },
+
+  {
+    path: "/vana",
+    name: "vana",
+    component: () => import("../src/pages/vana.vue"),
+  },
 ];
 
 export const router = createRouter({
